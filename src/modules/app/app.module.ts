1527979c89f10fec04998182e82bf75cf381ef12
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-01-23 12:34:51 -0500

    Se crea interfacede Usuarios y Cuentas
    Se crea carpeta interface y dentro de ellas las carpetas de user y account, cada una de ellas con su correspondiente docuemento interface.

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from 'src/controllers';
+import { AccountController } from 'src/controllers/account/account.controller';
 import { UserController } from 'src/controllers/user/user.controller';
 import { AccountService } from 'src/services';
 
 @Module({
   imports: [],
-  controllers: [SecurityController, UserController],
+  controllers: [SecurityController, UserController,AccountController],
   providers: [AccountService],
 })
 export class AppModule {}
