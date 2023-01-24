dc3ebab1630d85977d4754a29d5fa2fedc35279f
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-01-24 15:46:36 -0500

    Se modifica el cascaron de los repositorios
    Se agrega informacion al cascaron de los repositorios.

--- src/persistence/repositories/account-type.repository.ts
+++ src/persistence/repositories/account-type.repository.ts
@@ -8,4 +8,24 @@ export class AccountTypeRepository {
     constructor() {
         this.database = new Array<AccountTypeEntity>();
     }
+
+    register(entity: AccountTypeEntity): AccountTypeEntity {
+        throw new Error('This method is not implemented');
+    }
+
+    update(id: string, entity: AccountTypeEntity): AccountTypeEntity {
+        throw new Error('This method is not implemented');
+    }
+
+    delete(id: string, soft?: boolean): void {
+        throw new Error('This method is not implemented');
+    }
+
+    findAll(): AccountTypeEntity[] {
+        throw new Error('This method is not implemented');
+    }
+
+    findOneById(id: string): AccountTypeEntity {
+        throw new Error('This method is not implemented');
+    }
 }
--- src/persistence/repositories/transfer.repository.ts
+++ src/persistence/repositories/transfer.repository.ts
@@ -8,4 +8,23 @@ export class TransferRespository {
     constructor() {
         this.database = new Array<TransferEntity>();
     }
+    register(entity: TransferEntity): TransferEntity {
+        throw new Error('This method is not implemented');
+    }
+
+    update(id: string, entity: TransferEntity): TransferEntity {
+        throw new Error('This method is not implemented');
+    }
+
+    delete(id: string, soft?: boolean): void {
+        throw new Error('This method is not implemented');
+    }
+
+    findAll(): TransferEntity[] {
+        throw new Error('This method is not implemented');
+    }
+
+    findOneById(id: string): TransferEntity {
+        throw new Error('This method is not implemented');
+    }
 }
