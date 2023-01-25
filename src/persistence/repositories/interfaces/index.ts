0752bf262ccd3799a227fa63f3bcba754351f7e2
ERuiz <edwar_775@hotmail.com>
2023-01-24 16:12:27 -0800

    Creación de interfaces
    Se crean todas las 6 interfaces para poder gestionar luego los repositorios con los métodos que debe devolver y luego usar las entidades que se necesiten, al principio se usó un método importando sus entidades pero dado que la interfaz debe ser genérica se resolvió usar lo que recibe cada método y que retorne una entidad genérica como lo es T en todos los casos

---
+++ src/persistence/repositories/interfaces/index.ts
@@ -0,0 +1,6 @@
+export * from './account-type.interface';
+export * from './account.interface';
+export * from './customer.interface';
+export * from './deposit.interface';
+export * from './document-type.interface';
+export * from './transfer.interface';
