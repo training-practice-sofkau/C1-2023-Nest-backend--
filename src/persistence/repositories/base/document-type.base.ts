1bc5b10cb035b27b0981f7cd583b95cbe20a2d86
ERuiz <edwar_775@hotmail.com>
2023-01-24 16:32:46 -0800

    Creación de clases abstractas en Base
    Se crearon las clases abstractas que implementan las interfaces creadas en el último commit, estas serán usadas para implementarlas en las entidades finalmente

---
+++ src/persistence/repositories/base/document-type.base.ts
@@ -0,0 +1,26 @@
+import { DocumentTypeInterface } from '../interfaces';
+
+export abstract class DocumentTypeBase<entity>
+  implements DocumentTypeInterface<entity>
+{
+  private readonly database: Array<entity>;
+
+  constructor() {
+    this.database = new Array<entity>();
+  }
+  register(entity: entity): entity {
+    throw new Error('Method not implemented.');
+  }
+  upate(id: string, entity: entity): entity {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: string, soft?: boolean | undefined): void {
+    throw new Error('Method not implemented.');
+  }
+  findAll(): entity[] {
+    throw new Error('Method not implemented.');
+  }
+  findOneById(id: string): entity {
+    throw new Error('Method not implemented.');
+  }
+}
