f225cd0fd8b1da19775e2b5d94f4a15caa01971f
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-01-25 18:43:15 -0500

    Se agrega funcionalidad a document Type
    Se agrega funcionalidad a los metodos de document Type repository, ademas se agrega un metodo adicional en la clase interface repository de document type.

--- src/persistence/repositories/document-type.repository.ts
+++ src/persistence/repositories/document-type.repository.ts
@@ -6,20 +6,49 @@ import { DocumentTypeRepositoryInterface } from './interface/document-type/docum
 @Injectable()
 export class DocumentTypeRepository
   extends BodyRepositoryAbstract<DocumentTypeEntity>
-  implements DocumentTypeRepositoryInterface {
-    register(entity: DocumentTypeEntity): DocumentTypeEntity {
-        throw new Error('This method is not implemented');
-    }
-    update(id: string, entity: DocumentTypeEntity): DocumentTypeEntity {
-        throw new Error('This method is not implemented');
-    }
-    delete(id: string, soft?: boolean | undefined): void {
-        throw new Error('This method is not implemented');
-    }
-    findAll(): DocumentTypeEntity[] {
-        throw new Error('This method is not implemented');
-    }
-    findOneById(id: string): DocumentTypeEntity {
-        throw new Error('This method is not implemented');
-    }
+  implements DocumentTypeRepositoryInterface
+{
+  register(entity: DocumentTypeEntity): DocumentTypeEntity {
+    this.database.push(entity);
+    const documentTypeIndex = this.database.findIndex(
+      (customer) => customer.id === entity.id,
+    );
+    return this.database[documentTypeIndex];
   }
+  update(id: string, entity: DocumentTypeEntity): DocumentTypeEntity {
+    const documentTypeIndex = this.database.findIndex(
+      (customer) => customer.id === id,
+    );
+    const data = this.database[documentTypeIndex];
+    this.database[documentTypeIndex] = {
+      ...data,
+      ...entity,
+      id: id,
+    };
+    return this.database[documentTypeIndex];
+  }
+  delete(id: string, soft?: boolean | undefined): void {
+    const documentTypeIndex = this.database.findIndex(
+      (customer) => customer.id === id,
+    );
+    this.database.splice(documentTypeIndex, 1);
+  }
+  findAll(): DocumentTypeEntity[] {
+    return this.database;
+  }
+  findOneById(id: string): DocumentTypeEntity {
+    const documentTypeIndex = this.database.findIndex(
+      (customer) => customer.id === id,
+    );
+    return this.database[documentTypeIndex];
+  }
+  findByState(state: boolean): DocumentTypeEntity[] {
+    let arrayState: DocumentTypeEntity[] = [];
+    this.database.map((documentType) => {
+      if (documentType.state === state) {
+        arrayState.push(documentType);
+      }
+    });
+    return arrayState;
+  }
+}
--- src/persistence/repositories/interface/document-type/document-type-repository.interface.ts
+++ src/persistence/repositories/interface/document-type/document-type-repository.interface.ts
@@ -1,4 +1,7 @@
 import { DocumentTypeEntity } from 'src/persistence/entities';
 import { BodyRepositoryInterface } from '../model-repository.interface';
 
-export interface DocumentTypeRepositoryInterface extends BodyRepositoryInterface<DocumentTypeEntity> {}
+export interface DocumentTypeRepositoryInterface
+  extends BodyRepositoryInterface<DocumentTypeEntity> {
+  findByState(state: boolean): DocumentTypeEntity[];
+}
