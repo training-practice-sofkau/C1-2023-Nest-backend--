2f86273d45c5ed03b194aa6e5a7e64d742aab117
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-01-25 20:25:32 -0500

    Se agrega funcionalidad a Deposit
    Se agrega la funcionalidad de los metodos de deposit repository, ademas se agregan nuevos metodos dentro de deposit interface repository.

--- src/persistence/repositories/deposit.repository.ts
+++ src/persistence/repositories/deposit.repository.ts
@@ -1,23 +1,94 @@
-import { Injectable } from "@nestjs/common";
-import { DepositEntity } from "../entities";
-import { BodyRepositoryAbstract } from "./base/base.repository";
-import { DepositRepositoryInterface } from "./interface/deposit/deposit-repository.interface";
+import { Injectable } from '@nestjs/common';
+import { DepositEntity } from '../entities';
+import { BodyRepositoryAbstract } from './base/base.repository';
+import { DepositRepositoryInterface } from './interface/deposit/deposit-repository.interface';
 
 @Injectable()
-export class DepositRepository extends BodyRepositoryAbstract<DepositEntity> implements DepositRepositoryInterface {
+export class DepositRepository
+  extends BodyRepositoryAbstract<DepositEntity>
+  implements DepositRepositoryInterface
+{
   register(entity: DepositEntity): DepositEntity {
-    throw new Error('This method is not implemented');
+    this.database.push(entity);
+    const depositIndex = this.database.findIndex(
+      (deposit) => deposit.id === entity.id,
+    );
+    return this.database[depositIndex];
   }
   update(id: string, entity: DepositEntity): DepositEntity {
-    throw new Error('This method is not implemented');
+    const depositIndex = this.database.findIndex(
+      (deposit) => deposit.id === id,
+    );
+    const data = this.database[depositIndex];
+    this.database[depositIndex] = {
+      ...data,
+      ...entity,
+      id: id,
+    };
+    return this.database[depositIndex];
   }
   delete(id: string, soft?: boolean | undefined): void {
-    throw new Error('This method is not implemented');
+    const depositIndex = this.database.findIndex(
+      (deposit) => deposit.id === id,
+    );
+    this.database.splice(depositIndex, 1);
   }
   findAll(): DepositEntity[] {
-    throw new Error('This method is not implemented');
+    return this.database;
   }
   findOneById(id: string): DepositEntity {
-    throw new Error('This method is not implemented');
+    const depositIndex = this.database.findIndex(
+      (deposit) => deposit.id === id,
+    );
+    return this.database[depositIndex];
+  }
+  findByAccountId(accountId: string): DepositEntity {
+    const depositIndex = this.database.findIndex(
+      (deposit) => deposit.accountId.id === accountId,
+    );
+    return this.database[depositIndex];
+  }
+  findByAccountTypeId(accountTypeId: string): DepositEntity {
+    const depositIndex = this.database.findIndex(
+      (deposit) => deposit.accountId.accountType.id === accountTypeId,
+    );
+    return this.database[depositIndex];
+  }
+  findByCustomerId(customerId: string): DepositEntity {
+    const depositIndex = this.database.findIndex(
+      (deposit) => deposit.accountId.customerId.id === customerId,
+    );
+    return this.database[depositIndex];
+  }
+  findByEmail(email: string): DepositEntity {
+    const depositIndex = this.database.findIndex(
+      (deposit) => deposit.accountId.customerId.email === email,
+    );
+    return this.database[depositIndex];
+  }
+  findByDocumentTypeId(documentTypeId: string): DepositEntity {
+    const depositIndex = this.database.findIndex(
+      (deposit) =>
+        deposit.accountId.customerId.documentType.id === documentTypeId,
+    );
+    return this.database[depositIndex];
+  }
+  findAmountGreaterThan(amount: number): DepositEntity[] {
+    let arrayAmount: DepositEntity[] = [];
+    this.database.map((deposit) => {
+      if (deposit.amount > amount) {
+        arrayAmount.push(deposit);
+      }
+    });
+    return arrayAmount;
+  }
+  findAmountLessThan(amount: number): DepositEntity[] {
+    let arrayAmount: DepositEntity[] = [];
+    this.database.map((deposit) => {
+      if (deposit.amount < amount) {
+        arrayAmount.push(deposit);
+      }
+    });
+    return arrayAmount;
   }
 }
