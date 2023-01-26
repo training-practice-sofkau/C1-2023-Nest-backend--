d505b631c951ef35aeebca62544e307c66e1f1d4
Julian362 <Juliangarcia362@hotmail.com>
2023-01-26 16:31:52 -0500

    Implementación de metodos account-type
    Se agregan los metodos base register, findAll, update, delete, findById y los metodos unicos de la clase:
    - findByState
    - findByName

--- src/persistence/repositories/account-type.repository.ts
+++ src/persistence/repositories/account-type.repository.ts
@@ -1,30 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { AccountTypeEntity as AccountTypeEntity } from '../entities';
 import { BaseRepository } from './base/';
 import { BaseRepositoryInterface } from './interfaces/';
-
 @Injectable()
 export class CustomerRepository
   extends BaseRepository<AccountTypeEntity>
   implements BaseRepositoryInterface<AccountTypeEntity>
 {
   register(entity: AccountTypeEntity): AccountTypeEntity {
-    throw new Error('This method is not implemented');
+    this.database.push(entity);
+    return this.database.at(-1) ?? entity;
   }
 
   update(id: string, entity: AccountTypeEntity): AccountTypeEntity {
-    throw new Error('This method is not implemented');
+    const index = this.database.findIndex((item) => item.id === id);
+    const data = this.database[index];
+    if (index >= 0) {
+      this.database[index] = {
+        ...data,
+        ...entity,
+        id,
+      } as AccountTypeEntity;
+    } else {
+      throw new NotFoundException(`El Id: ${id} no existe en base de datos`);
+    }
+    return this.database[index];
   }
 
   delete(id: string, soft?: boolean): void {
-    throw new Error('This method is not implemented');
+    const index = this.database.findIndex((item) => item.id === id);
+    this.database.splice(index, 1);
   }
 
   findAll(): AccountTypeEntity[] {
-    throw new Error('This method is not implemented');
+    return this.findByState(true);
   }
 
   findOneById(id: string): AccountTypeEntity {
-    throw new Error('This method is not implemented');
+    const index = this.database.findIndex((item) => item.id === id);
+    if (index >= 0) {
+      return this.database[index];
+    } else {
+      throw new NotFoundException(`El Id: ${id} no existe en base de datos`);
+    }
+  }
+
+  findByState(state: boolean): AccountTypeEntity[] {
+    return this.database.filter((item) => item.state === state);
+  }
+
+  findByName(name: string): AccountTypeEntity[] {
+    return this.database.filter((item) => item.name === name);
   }
 }
