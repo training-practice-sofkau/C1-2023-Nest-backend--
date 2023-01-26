43d4be8c9b908b92139508140b8133d54572fb87
Julian362 <Juliangarcia362@hotmail.com>
2023-01-26 16:36:27 -0500

    Implementación de metodos  customer
    Se agregan los metodos base register, findAll, update, delete, findOneById y los metodos unicos de la clase:
    
    - findByEmailAndPassword
    - findByDocumentAndTypeDocument
    - findByOneEmail
    - findByOnePhone
    - findByState
    - findByFullName

--- src/persistence/repositories/customer.repository.ts
+++ src/persistence/repositories/customer.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CustomerEntity } from '../entities';
 import { BaseRepository } from './base/';
 import { BaseRepositoryInterface } from './interfaces/';
@@ -9,18 +9,110 @@ export class CustomerRepository
   implements BaseRepositoryInterface<CustomerEntity>
 {
   register(entity: CustomerEntity): CustomerEntity {
-    throw new Error('Method not implemented.');
+    this.database.push(entity);
+    return this.database.at(-1) ?? entity;
   }
+
   update(id: string, entity: CustomerEntity): CustomerEntity {
-    throw new Error('Method not implemented.');
+    const index = this.database.findIndex(
+      (item) => item.id === id && (item.deletedAt ?? true) === true,
+    );
+    if (index >= 0) {
+      this.database[index] = {
+        ...this.database[index],
+        ...entity,
+        id,
+      } as CustomerEntity;
+    } else {
+      throw new NotFoundException(`El ID ${id} no existe en base de datos`);
+    }
+    return this.database[index];
   }
-  delete(id: string, soft?: boolean | undefined): void {
-    throw new Error('Method not implemented.');
+
+  delete(id: string, soft?: boolean): void {
+    const customer = this.findOneById(id);
+    if (soft || soft === undefined) {
+      customer.deletedAt = Date.now();
+      this.update(id, customer);
+    } else {
+      const index = this.database.findIndex(
+        (item) => item.id === id && (item.deletedAt ?? true) === true,
+      );
+      this.database.splice(index, 1);
+    }
   }
+
   findAll(): CustomerEntity[] {
-    throw new Error('Method not implemented.');
+    return this.database.filter((item) => item.deletedAt === undefined);
   }
+
   findOneById(id: string): CustomerEntity {
-    throw new Error('Method not implemented.');
+    const customer = this.database.find(
+      (item) => item.id === id && (item.deletedAt ?? true) === true,
+    );
+    if (customer) return customer;
+    else throw new NotFoundException(`El ID ${id} no existe en base de datos`);
+  }
+
+  findOneByEmailAndPassword(email: string, password: string): boolean {
+    const index = this.database.findIndex(
+      (item) =>
+        item.email === email &&
+        item.password === password &&
+        typeof item.deletedAt === 'undefined',
+    );
+    return index >= 0 ? true : false;
+  }
+
+  findOneByDocumentTypeAndDocument(
+    documentTypeId: string,
+    document: string,
+  ): CustomerEntity {
+    const index = this.database.findIndex(
+      (item) =>
+        item.documentType.id === documentTypeId &&
+        item.document === document &&
+        typeof item.deletedAt === 'undefined',
+    );
+    if (index >= 0) {
+      throw new NotFoundException(`no existe en base de datos`);
+    }
+    const customer = this.database[index];
+    return customer;
+  }
+
+  findOneByEmail(email: string): CustomerEntity {
+    const index = this.database.findIndex(
+      (item) => item.email === email && typeof item.deletedAt === 'undefined',
+    );
+    if (index >= 0) {
+      throw new NotFoundException(`no existe en base de datos`);
+    }
+    const customer = this.database[index];
+    return customer;
+  }
+
+  findOneByPhone(phone: string): CustomerEntity {
+    const index = this.database.findIndex(
+      (item) => item.phone === phone && typeof item.deletedAt === 'undefined',
+    );
+    if (index >= 0) {
+      throw new NotFoundException(`no existe en base de datos`);
+    }
+    const customer = this.database[index];
+    return customer;
+  }
+
+  findByState(state: boolean): CustomerEntity[] {
+    return this.database.filter(
+      (item) => item.state === state && typeof item.deletedAt === 'undefined',
+    );
+  }
+
+  findByFullName(fullName: string): CustomerEntity[] {
+    return this.database.filter(
+      (item) =>
+        item.fullName === fullName && typeof item.deletedAt === 'undefined',
+    );
   }
 }
