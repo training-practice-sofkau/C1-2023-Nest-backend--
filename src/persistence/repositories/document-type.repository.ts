f87d8a82a6d9d6b7433558c8ae4520bf9684f349
Julian362 <Juliangarcia362@hotmail.com>
2023-01-26 16:38:16 -0500

    Implementación de metodos  document-tye
    Se agregan los metodos base register, findAll, update, delete, findOneById y los metodos unicos de la clase:
    
    - findByState
    - findByName

--- src/persistence/repositories/document-type.repository.ts
+++ src/persistence/repositories/document-type.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { DocumentTypeEntity } from '../entities';
 import { BaseRepository } from './base/';
 import { BaseRepositoryInterface } from './interfaces/';
@@ -9,18 +9,45 @@ export class DocumentTypeRepository
   implements BaseRepositoryInterface<DocumentTypeEntity>
 {
   register(entity: DocumentTypeEntity): DocumentTypeEntity {
-    throw new Error('Method not implemented.');
+    this.database.push(entity);
+    return this.database.at(-1) ?? entity;
   }
+
   update(id: string, entity: DocumentTypeEntity): DocumentTypeEntity {
-    throw new Error('Method not implemented.');
+    const index = this.database.findIndex((item) => item.id === id);
+    const data = this.database[index];
+    if (index >= 0) {
+      this.database[index] = {
+        ...data,
+        ...entity,
+        id,
+      } as DocumentTypeEntity;
+    } else {
+      throw new NotFoundException(`El Id: ${id} no existe en base de datos`);
+    }
+    return this.database[index];
   }
-  delete(id: string, soft?: boolean | undefined): void {
-    throw new Error('Method not implemented.');
+
+  delete(id: string, soft?: boolean): void {
+    const index = this.database.findIndex((item) => item.id === id);
+    this.database.splice(index, 1);
   }
+
   findAll(): DocumentTypeEntity[] {
-    throw new Error('Method not implemented.');
+    return this.findByState(true);
   }
+
   findOneById(id: string): DocumentTypeEntity {
-    throw new Error('Method not implemented.');
+    const documentType = this.database.find((item) => item.id === id);
+    if (documentType) return documentType;
+    else throw new NotFoundException(`El ID ${id} no existe en base de datos`);
+  }
+
+  findByState(state: boolean): DocumentTypeEntity[] {
+    return this.database.filter((item) => item.state === state);
+  }
+
+  findByName(name: string): DocumentTypeEntity[] {
+    return this.database.filter((item) => item.name === name);
   }
 }
