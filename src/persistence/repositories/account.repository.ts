a4a54dd72320b110d70f89998eeb0b90e13d9bf9
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-01-26 15:14:28 -0500

    logica del repositorio de 'account' - implentar los 5 metodos comunes para todos y agregar los unicos

--- src/persistence/repositories/account.repository.ts
+++ src/persistence/repositories/account.repository.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CustomerEntity } from '../entities';
 import { AccountEntity } from '../entities/account.entity';
 import { BaseRepository } from './base';
 import { AccountRepositoryInterface } from './interfaces';
@@ -9,39 +10,151 @@ export class AccountRepository
   implements AccountRepositoryInterface
 {
   register(entity: AccountEntity): AccountEntity {
-    throw new Error('This method is not implemented');
+    this.database.push(entity);
+    return this.database.at(-1) ?? entity;
   }
 
   update(id: string, entity: AccountEntity): AccountEntity {
-    throw new Error('This method is not implemented');
+    const index = this.database.findIndex(
+      (item) => item.id === id && (item.deletedAt ?? true) === true,
+    );
+    if (index >= 0) {
+      this.database[index] = {
+        ...this.database[index],
+        ...entity,
+        id,
+      } as AccountEntity;
+    } else {
+      throw new NotFoundException(`El ID ${id} no existe en base de datos`);
+    }
+    return this.database[index];
   }
 
   delete(id: string, soft?: boolean): void {
-    throw new Error('This method is not implemented');
+    const customer = this.findOneById(id);
+    if (soft || soft === undefined) {
+      customer.deletedAt = Date.now();
+      this.update(id, customer);
+    } else {
+      const index = this.database.findIndex(
+        (item) => item.id === id && (item.deletedAt ?? true) === true,
+      );
+      this.database.splice(index, 1);
+    }
   }
 
   findAll(): AccountEntity[] {
-    throw new Error('This method is not implemented');
+    return this.database.filter((item) => item.deletedAt === undefined);
   }
 
   findOneById(id: string): AccountEntity {
-    throw new Error('This method is not implemented');
+    const customer = this.database.find(
+      (item) => item.id === id && (item.deletedAt ?? true) === true,
+    );
+    if (customer) return customer;
+    else throw new NotFoundException(`El ID ${id} no existe en base de datos`);
   }
 
   findByState(state: boolean): AccountEntity[] {
-    throw new Error('Method not implemented.');
+    const accounts = this.database.filter((item) => item.state === state);
+    if (accounts) return accounts;
+    return [];
   }
+
   findByCustomer(customerId: string): AccountEntity[] {
-    throw new Error('Method not implemented.');
+    const accounts = this.database.filter(
+      (item) => item.customer.id === customerId,
+    );
+    if (accounts) return accounts;
+    return [];
   }
+
   findByAccountType(accountTypeId: string): AccountEntity[] {
-    throw new Error('Method not implemented.');
+    const accounts = this.database.filter(
+      (item) => item.accountType.id === accountTypeId,
+    );
+    if (accounts) return accounts;
+    return [];
   }
   private hardDelete(index: number): void {
-    throw new Error('This method is not implemented');
+    this.database.splice(index, 1);
   }
 
   private softDelete(index: number): void {
-    throw new Error('This method is not implemented');
+    this.database[index].deletedAt = Date.now();
+    this.update(this.database[index].id, this.database[index]);
   }
 }
+
+/*const dataAccountType: any[] = [
+  {
+    id: '1',
+    name: 'corriente1',
+    state: true,
+    deletedAt: undefined,
+  },
+  {
+    id: '2',
+    name: 'corriente2',
+    state: true,
+    deletedAt: 789,
+  },
+];
+
+function update(id: string, entity: any): any {
+  const index = dataAccountType.findIndex((item: AccountEntity) => {
+    item.id === id && (item.deletedAt ?? true) === true;
+  });
+  if (index >= 0) {
+    dataAccountType[index] = {
+      ...dataAccountType[index],
+      ...entity,
+      id,
+    } as any;
+  } else {
+    console.log(`El ID ${id} no existe en base de datos`);
+    //throw new NotFoundException(`El ID ${id} no existe en base de datos`);
+  }
+  return dataAccountType[index];
+}
+
+const u: any = {
+  id: '1',
+  name: 'UPDATE',
+  state: true,
+  //deletedAt: undefined,
+};
+
+//console.log(update('2', u));
+console.log('data ', dataAccountType);
+//console.log(false === true);
+
+function deletee(id: string, soft?: boolean): void {
+  const item = findOneById(id);
+  console.log('soft ', soft);
+  console.log('soft ', soft || soft);
+  console.log('soft ', soft || soft === undefined); //undefined === undefined, si no le paso nada de soft
+  //true === undefined, si le paso un true
+  if (soft || soft === undefined) {
+    console.log('entra en if');
+    item.deletedAt = Date.now();
+    update(id, item);
+  } else {
+    console.log('entra en else');
+    const index = dataAccountType.findIndex(
+      (item) => item.id === id && (item.deletedAt ?? true) === true,
+    );
+    dataAccountType.splice(index, 1);
+  }
+  console.log('data ', dataAccountType);
+}
+
+function findOneById(id: string): any {
+  const customer = dataAccountType.find(
+    (item: any) => item.id === id && (item.deletedAt ?? true) === true,
+  );
+  if (customer) return customer;
+  else throw new NotFoundException(`El ID ${id} no existe en base de datos`);
+}
+
+deletee('1', true);*/
