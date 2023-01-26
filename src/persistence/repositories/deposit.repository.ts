af9f28bdb167dda88080b1fdaf2cc113e591c871
Julian362 <Juliangarcia362@hotmail.com>
2023-01-26 16:37:34 -0500

    Implementación de metodos deposit
    Se agregan los metodos base register, findAll, update, delete, findOneById y los metodos unicos de la clase:
    
    - hardDelete
    - softDelete
    - findByAccountId
    - findByDataRange

--- src/persistence/repositories/deposit.repository.ts
+++ src/persistence/repositories/deposit.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { DepositEntity } from '../entities';
 import { BaseRepository } from './base/';
 import { BaseRepositoryInterface } from './interfaces/';
@@ -9,18 +9,73 @@ export class CustomerRepository
   implements BaseRepositoryInterface<DepositEntity>
 {
   register(entity: DepositEntity): DepositEntity {
-    throw new Error('Method not implemented.');
+    this.database.push(entity);
+    return this.database.at(-1) ?? entity;
   }
+
   update(id: string, entity: DepositEntity): DepositEntity {
-    throw new Error('Method not implemented.');
+    const index = this.database.findIndex(
+      (item) => item.id === id && (item.deletedAt ?? true) === true,
+    );
+    if (index >= 0) {
+      this.database[index] = {
+        ...this.database[index],
+        ...entity,
+        id,
+      } as DepositEntity;
+    } else {
+      throw new NotFoundException(`El ID ${id} no existe en base de datos`);
+    }
+    return this.database[index];
   }
-  delete(id: string, soft?: boolean | undefined): void {
-    throw new Error('Method not implemented.');
+
+  delete(id: string, soft?: boolean): void {
+    this.findOneById(id);
+    if (soft || soft === undefined) {
+      const index = this.database.findIndex((item) => item.id === id);
+      this.softDelete(index);
+    } else {
+      const index = this.database.findIndex(
+        (item) => item.id === id && (item.deletedAt ?? true) === true,
+      );
+      this.hardDelete(index);
+    }
   }
+
+  private hardDelete(index: number): void {
+    this.database.splice(index, 1);
+  }
+
+  private softDelete(index: number): void {
+    const account = this.database[index];
+    account.deletedAt = Date.now();
+    this.update(account.id, account);
+  }
+
   findAll(): DepositEntity[] {
-    throw new Error('Method not implemented.');
+    return this.database.filter((item) => item.deletedAt === undefined);
   }
+
   findOneById(id: string): DepositEntity {
-    throw new Error('Method not implemented.');
+    const account = this.database.find(
+      (item) => item.id === id && (item.deletedAt ?? true) === true,
+    );
+    if (account) return account;
+    else throw new NotFoundException(`El ID ${id} no existe en base de datos`);
+  }
+
+  findByAccountId(accountId: string): DepositEntity[] {
+    return this.database.filter(
+      (item) => item.account.id === accountId && item.deletedAt === undefined,
+    );
+  }
+
+  findByDataRange(
+    dateInit: Date | number,
+    dateEnd: Date | number,
+  ): DepositEntity[] {
+    return this.database.filter(
+      (item) => item.dateTime <= dateInit && item.dateTime >= dateEnd,
+    );
   }
 }
