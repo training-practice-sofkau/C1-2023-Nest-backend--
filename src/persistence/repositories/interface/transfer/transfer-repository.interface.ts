75046e8a379be8f06f5181056e7191724ab0822c
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-01-25 21:34:38 -0500

    Se agrega funcionalidad Transfer
    Se agrega funcionalidad a los metodos a transfer repository, ademas se adicionan metodos para aumentar la funcionalidad en el transfer repository interface.

--- src/persistence/repositories/interface/transfer/transfer-repository.interface.ts
+++ src/persistence/repositories/interface/transfer/transfer-repository.interface.ts
@@ -1,4 +1,12 @@
-import { TransferEntity } from "src/persistence/entities";
-import { BodyRepositoryInterface } from "../model-repository.interface";
+import { TransferEntity } from 'src/persistence/entities';
+import { BodyRepositoryInterface } from '../model-repository.interface';
 
-export interface TransferRepositoryInterface extends BodyRepositoryInterface<TransferEntity>{}
+export interface TransferRepositoryInterface
+  extends BodyRepositoryInterface<TransferEntity> {
+  findByIncomeId(id: string): TransferEntity;
+  findByOutcomeId(id: string): TransferEntity;
+  findByIncomeCustomerId(id: string): TransferEntity;
+  findByOutcomeCustomerId(id: string): TransferEntity;
+  findByAmountGreaterThan(amount: number): TransferEntity[];
+  findByAmountLessThan(amount: number): TransferEntity[];
+}
--- src/persistence/repositories/transfer.repository.ts
+++ src/persistence/repositories/transfer.repository.ts
@@ -6,20 +6,82 @@ import { TransferRepositoryInterface } from './interface/transfer/transfer-repos
 @Injectable()
 export class TransferRespository
   extends BodyRepositoryAbstract<TransferEntity>
-  implements TransferRepositoryInterface {
-    register(entity: TransferEntity): TransferEntity {
-        throw new Error('This method is not implemented');
-    }
-    update(id: string, entity: TransferEntity): TransferEntity {
-        throw new Error('This method is not implemented');
-    }
-    delete(id: string, soft?: boolean | undefined): void {
-        throw new Error('This method is not implemented');
-    }
-    findAll(): TransferEntity[] {
-        throw new Error('This method is not implemented');
-    }
-    findOneById(id: string): TransferEntity {
-        throw new Error('This method is not implemented');
-    }
+  implements TransferRepositoryInterface
+{
+  register(entity: TransferEntity): TransferEntity {
+    this.database.push(entity);
+    const transferIndex = this.database.findIndex(
+      (transfer) => transfer.id === entity.id,
+    );
+    return this.database[transferIndex];
   }
+  update(id: string, entity: TransferEntity): TransferEntity {
+    const transferIndex = this.database.findIndex(
+      (transfer) => transfer.id === id,
+    );
+    const data = this.database[transferIndex];
+    this.database[transferIndex] = {
+      ...data,
+      ...entity,
+      id: id,
+    };
+    return this.database[transferIndex];
+  }
+  delete(id: string, soft?: boolean | undefined): void {
+    const transferIndex = this.database.findIndex(
+      (transfer) => transfer.id === id,
+    );
+    this.database.splice(transferIndex, 1);
+  }
+  findAll(): TransferEntity[] {
+    return this.database;
+  }
+  findOneById(id: string): TransferEntity {
+    const transferIndex = this.database.findIndex(
+      (transfer) => transfer.id === id,
+    );
+    return this.database[transferIndex];
+  }
+  findByIncomeCustomerId(id: string): TransferEntity {
+    const transferIndex = this.database.findIndex(
+      (transfer) => transfer.income.customerId.id === id,
+    );
+    return this.database[transferIndex];
+  }
+  findByIncomeId(id: string): TransferEntity {
+    const transferIndex = this.database.findIndex(
+      (transfer) => transfer.income.id === id,
+    );
+    return this.database[transferIndex];
+  }
+  findByOutcomeId(id: string): TransferEntity {
+    const transferIndex = this.database.findIndex(
+      (transfer) => transfer.outcome.id === id,
+    );
+    return this.database[transferIndex];
+  }
+  findByOutcomeCustomerId(id: string): TransferEntity {
+    const transferIndex = this.database.findIndex(
+      (transfer) => transfer.outcome.customerId.id === id,
+    );
+    return this.database[transferIndex];
+  }
+  findByAmountGreaterThan(amount: number): TransferEntity[] {
+    let arrayAmount: TransferEntity[] = [];
+    this.database.map((transfer) => {
+      if (transfer.amount > amount) {
+        arrayAmount.push(transfer);
+      }
+    });
+    return arrayAmount;
+  }
+  findByAmountLessThan(amount: number): TransferEntity[] {
+    let arrayAmount: TransferEntity[] = [];
+    this.database.map((transfer) => {
+      if (transfer.amount < amount) {
+        arrayAmount.push(transfer);
+      }
+    });
+    return arrayAmount;
+  }
+}
