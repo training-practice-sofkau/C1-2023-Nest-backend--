86aca395621edfb95b041a460b95b99c1b9170cc
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-01-27 16:04:23 -0500

    crear servicio de security - hacer logica del servicio security haciendo falta el metodo *SignOut*

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services';
 import { AccountService } from './services/account/account.service';
+import { SecurityService } from './services/security/security.service';
 
 @Module({
   imports: [],
   controllers: [SecurityController],
-  providers: [AccountService],
+  providers: [AccountService, SecurityService],
 })
 export class AppModule {}
