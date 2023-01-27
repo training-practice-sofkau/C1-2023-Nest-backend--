e76a22b808ec7502d7cf66bc22ae35a062c85513
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-01-27 15:31:32 -0500

    crear servicio de deposit - hacer logica del servicio deposit haciendo falta el metodo *getHistory*

---
+++ src/services/deposit/deposit.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { DepositModel } from 'src/models';
+import { DepositEntity } from 'src/persistence/entities';
+import { DepositRepository } from 'src/persistence/repositories/deposit.repository';
+
+@Injectable()
+export class DepositService {
+  constructor(private readonly depositRepository: DepositRepository) {}
+
+  /**
+   * Crear un deposito
+   *
+   * @param {DepositModel} deposit
+   * @return {*}  {DepositEntity}
+   * @memberof DepositService
+   */
+  createDeposit(deposit: DepositModel): DepositEntity {
+    const newDeposit = new DepositEntity();
+    newDeposit.account = deposit.account;
+    return this.depositRepository.register(newDeposit);
+  }
+
+  /**
+   * Borrar un deposito
+   *
+   * @param {string} depositId
+   * @memberof DepositService
+   */
+  deleteDeposit(depositId: string): void {
+    this.depositRepository.delete(depositId);
+  }
+
+  /**
+   * Obtener el historial de los depósitos en una cuenta
+   *
+   * @param {string} accountId
+   * @param {PaginationModel} pagination
+   * @param {DataRangeModel} [dataRange]
+   * @return {*}  {DepositEntity[]}
+   * @memberof DepositService
+   */
+  getHistory(
+    accountId: string,
+    //pagination: PaginationModel,
+    //dataRange?: DataRangeModel,
+  ): DepositEntity[] {
+    throw new Error('This method is not implemented');
+  }
+}
