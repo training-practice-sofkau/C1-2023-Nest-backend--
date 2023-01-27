17aea2ee5d4622a24ac66730493a13a1918b0ae3
migueltorresd <migueltorresdiaz1992@gmail.com>
2023-01-27 16:12:06 -0500

    Titulo: primeros metodos account  -Descripcion: se crean los primeros metodos del servicio de acount

--- src/services/account/account.service.ts
+++ src/services/account/account.service.ts
@@ -1,4 +1,138 @@
 import { Injectable } from '@nestjs/common';
+import { ForbiddenException } from '@nestjs/common/exceptions';
+
+import { AccountModel } from 'src/models';
+import { AccountEntity, AccountTypeEntity } from 'src/persistence/entities';
+import { AccountTypeRepository } from 'src/persistence/repositories/account-type.repository';
+import { AccountRepository } from 'src/persistence/repositories/account.repository';
 
 @Injectable()
-export class AccountService {}
+export class AccountService {
+  constructor(
+    private readonly accountRepository: AccountRepository,
+    private readonly accountTypeRepository: AccountTypeRepository,
+  ) {}
+
+  /**
+   * Crear una cuenta
+   *
+   * @param {AccountModel} account
+   * @return {*}  {AccountEntity}
+   * @memberof AccountService
+   */
+  createAccount(account: AccountModel): AccountEntity {
+    const newAccount = new AccountEntity();
+    newAccount.customer = account.customer;
+    newAccount.accountType = account.accountType;
+    return this.accountRepository.register(newAccount);
+  }
+
+  /**
+   * Obtener el balance de una cuenta
+   *
+   * @param {string} accountId
+   * @return {*}  {number}
+   * @memberof AccountService
+   */
+  getBalance(accountId: string): number {
+    return this.accountRepository.findOneById(accountId).balance;
+  }
+
+  /**
+   * Agregar balance a una cuenta
+   *
+   * @param {string} accountId
+   * @param {number} amount
+   * @memberof AccountService
+   */
+  addBalance(accountId: string, amount: number): void {
+    this.accountRepository.findOneById(accountId).balance += amount; //se va asuma el monto al balance
+  }
+
+  /**
+   * Remover balance de una cuenta
+   *
+   * @param {string} accountId
+   * @param {number} amount
+   * @memberof AccountService
+   */
+  removeBalance(accountId: string, amount: number): void {
+    if (this.verifyAmountIntoBalance(accountId, amount)) {
+      this.accountRepository.findOneById(accountId).balance -= amount;
+    } else {
+      throw new Error(
+        'El valor que desea retirar no puede ser mayor al saldo que tiene en su cuenta',
+      );
+    }
+  }
+
+  /**
+   * Verificar la disponibilidad de un monto a retirar en una cuenta
+   *
+   * @param {string} accountId
+   * @param {number} amount
+   * @return {*}  {boolean}
+   * @memberof AccountService
+   */
+  verifyAmountIntoBalance(accountId: string, amount: number): boolean {
+    throw new Error('This method is not implemented');
+  }
+
+  /**
+   * Obtener el estado de una cuenta
+   *
+   * @param {string} accountId
+   * @return {*}  {boolean}
+   * @memberof AccountService
+   */
+  getState(accountId: string): boolean {
+    throw new Error('This method is not implemented');
+  }
+
+  /**
+   * Cambiar el estado de una cuenta
+   *
+   * @param {string} accountId
+   * @param {boolean} state
+   * @memberof AccountService
+   */
+  changeState(accountId: string, state: boolean): void {
+    throw new Error('This method is not implemented');
+  }
+
+  /**
+   * Obtener el tipo de cuenta de una cuenta
+   *
+   * @param {string} accountId
+   * @return {*}  {AccountTypeEntity}
+   * @memberof AccountService
+   */
+  getAccountType(accountId: string): AccountTypeEntity {
+    throw new Error('This method is not implemented');
+  }
+
+  /**
+   * Cambiar el tipo de cuenta a una cuenta
+   *
+   * @param {string} accountId
+   * @param {string} accountTypeId
+   * @return {*}  {AccountTypeEntity}
+   * @memberof AccountService
+   */
+  changeAccountType(
+    accountId: string,
+    accountTypeId: string,
+  ): AccountTypeEntity {
+    throw new Error('This method is not implemented');
+  }
+
+  /**
+   * Borrar una cuenta
+   *
+   * @param {string} accountId
+   * @memberof AccountService
+   */
+  deleteAccount(accountId: string): void {
+    throw new Error('This method is not implemented');
+  }
+}
--- src/services/deposit/deposit.service.ts
+++ src/services/deposit/deposit.service.ts
@@ -1,4 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { PaginationModel } from 'src/models/pagination.model';
+import { DepositEntity } from 'src/persistence';
+
+import { DepositModel } from '../../models/';
+import { DepositRepository } from '../../persistence/repositories/deposit.repository';
 
 @Injectable()
-export class DepositService {}
+export class DepositService {
+  [x: string]: any;
+  constructor(
+    private readonly DepositRepository: DepositRepository,
+  ) {}
+  /
+   * Crear un deposito
+   *
+   * @param {DepositModel} deposit
+   * @return {*}  {DepositEntity}
+   * @memberof DepositService
+   */
+  createDeposit(deposit: DepositModel): DepositEntity {
+   return this.DepositRepository.createDeposit(deposit);
+
+  }
+
+  /
+   * Borrar un deposito
+   *
+   * @param {string} depositId
+   * @memberof DepositService
+   */
+  deleteDeposit(depositId: string): void {
+   this.DepositRepository.delete(depositId)
+  }
+
+  /**
+   * Obtener el historial de los depósitos en una cuenta
+   *
+   * @param {string} depositId
+   * @param {PaginationModel} pagination
+   * @param {DataRangeModel} [dataRange]
+   * @return {*}  {DepositEntity[]}
+   * @memberof DepositService
+   */
+  getHistory(
+    depositId: string,
+    pagination?: PaginationModel,
+    dataRange?: DataRangeModel,
+  ): DepositEntity[] {
+     let deposit =  this.DepositRepository.searchDeposit("id", depositId)
+
+     if(pagination){
+      const {offset, limit } = pagination
+      deposit = deposit.slice(offset, offset + limit );
+     }
+     return deposit
+  }
+}
