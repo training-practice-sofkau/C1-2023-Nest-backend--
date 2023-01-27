90eb5c7126db1e48f237bfdfb5a65e70fde7b05c
ERuiz <edwar_775@hotmail.com>
2023-01-27 16:58:44 -0500

    Creación servicio de Customer
    Se agrega lógica solo de dos métodos para el servicio en cuestión.

--- src/services/customer/customer.service.ts
+++ src/services/customer/customer.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CustomerModel } from 'src/models';
+import { CustomerEntity } from 'src/persistence/entities';
+import { CustomerRepository } from 'src/persistence/repositories';
+import { AccountService } from '../account';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+  constructor(
+    private readonly customerRepository: CustomerRepository,
+    private readonly accountService: AccountService,
+  ) {}
+
+  getCustomerInfo(customerId: string): CustomerEntity {
+    const currentCustomer = this.customerRepository.findOneById(customerId);
+    return currentCustomer;
+  }
+
+  updatedCustomer(customerId: string, customer: CustomerModel): CustomerEntity {
+    return this.customerRepository.upate(customerId, customer);
+  }
+
+  unsubscribe(id: string): boolean {
+    throw new Error('Method not implemented.');
+  }
+}
