d19bccfe01532dbd65a0aa6721e3c1067e0e1617
Julian362 <Juliangarcia362@hotmail.com>
2023-01-27 17:08:22 -0500

    Se agregan metodos
    Se agregan métodos para DepositService

--- src/services/deposit/deposit.service.ts
+++ src/services/deposit/deposit.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { DepositModel } from '../../models';
+import { DepositEntity } from '../../persistence/entities';
+import { DepositRepository } from '../../persistence/repositories';
 
 @Injectable()
-export class DepositService {}
+export class DepositService {
+  constructor(private readonly depositRepository: DepositRepository) {}
+
+  /**
+   * Crear un deposito
+   *
+   * @param {DepositModel} deposit
+   * @return {*}  {DepositEntity}
+   * @memberof DepositService
+   */
+  createDeposit(deposit: DepositModel): DepositEntity {
+    const depositEntity = new DepositEntity();
+    depositEntity.account = deposit.account;
+    return this.depositRepository.register(depositEntity);
+  }
+
+  /**
+   * Borrar un deposito
+   *
+   * @param {string} depositId
+   * @memberof DepositService
+   */
+  deleteDeposit(depositId: string): void {
+    this.accountRepository.delete(accountId);
+  }
+
+  /**
+   * Obtener el historial de los depósitos en una cuenta
+   *
+   * @param {string} accountId
+   * @param {PaginationModel} pagination
+   * @param {DataRangeModel} [dataRange]
+   * @return {*}  {DepositEntity[]}
+   * @memberof DepositService
+   */
+  getHistory(
+    accountId: string,
+    pagination: PaginationModel,
+    dataRange?: DataRangeModel,
+  ): DepositEntity[] {
+    throw new Error('This method is not implemented');
+  }
+}
