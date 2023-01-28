6b763cce24c08cfe79fc7e022fb40f8c2eafe7c8
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-01-28 16:50:39 -0500

    Update customer.module.ts

--- src/modules/customer/customer.module.ts
+++ src/modules/customer/customer.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { UserController } from "src/controllers/customer/customer.controller";
+import { CustomerRepository } from "src/persistence/repositories/customer.repository";
 import { CustomerService } from "src/services/customer/customer.service";
 
 @Module({
+    imports:[],
     controllers:[UserController],
     providers:[CustomerService]
 })
