73e91e9cbaa2227d20b08d89f9995e59de24009d
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-01-28 18:04:32 -0500

    Cambios Customer y Account Service
    Se realiza cambios de los metodos dentro de los servicios de customer y account para mejorar o completar su funcionalidad.

--- src/modules/customer/customer.module.ts
+++ src/modules/customer/customer.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { UserController } from "src/controllers/customer/customer.controller";
-import { CustomerRepository } from "src/persistence/repositories/customer.repository";
 import { CustomerService } from "src/services/customer/customer.service";
 
 @Module({
