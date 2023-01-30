a771e65913d5592cbeb591c6da2f4385d1142dad
codeJ30 <juancode09@gmail.com>
2023-01-30 15:28:47 -0500

    TITULO: Se crea el controlador de usuario, DESCRIPTION: Elimino carpetas mal creadas y creo el primer controlador

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services/account/account.service';
+import { UserController } from './controllers/user/user.controller';
 
 @Module({
   imports: [],
-  controllers: [SecurityController],
+  controllers: [SecurityController, UserController],
   providers: [AccountService],
 })
 export class AppModule {}
