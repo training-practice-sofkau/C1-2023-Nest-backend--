9f7d7bd08b3ef5dd45c57dd2576b84ea4bf345c6
ERuiz <edwar_775@hotmail.com>
2023-01-29 21:23:48 -0500

    Creación de modelos pagination y datarange
    Se crean los modelos de pagination y datarange con el objetivo de poder implementar la paginación en los servicios de tranfers y deposits.

---
+++ src/models/pagination.model.ts
@@ -0,0 +1,5 @@
+export interface PaginationModel {
+  size: number;
+  pages: number;
+  currentPage: number;
+}
--- src/services/transfer/transfer.service.ts
+++ src/services/transfer/transfer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { TransferModel } from 'src/models';
+import { DataRangeModel, PaginationModel, TransferModel } from 'src/models';
 import { TransferEntity } from 'src/persistence/entities';
 import { TransferRepository } from 'src/persistence/repositories';
 import { AccountService } from '../account';
@@ -28,21 +28,60 @@ export class TransferService {
     accountId: string,
     pagination: PaginationModel,
     dataRange?: DataRangeModel,
-  ): TransferEntity[] {}
+  ): Promise<TransferEntity[]> {
+    const currentTransfers =
+      this.transferRepository.findByOutcomeAccount(accountId);
+    pagination.size = currentTransfers.length;
+    return this.historyPagination(currentTransfers, pagination, dataRange);
+  }
 
-  async getHistoryOut(
+  async getHistoryIn(
     accountId: string,
     pagination: PaginationModel,
     dataRange?: DataRangeModel,
-  ): TransferEntity[] {}
+  ): Promise<TransferEntity[]> {
+    const currentTransfers =
+      this.transferRepository.findByIncomeAccount(accountId);
+    return this.historyPagination(currentTransfers, pagination, dataRange);
+  }
 
   getHistory(
     accountId: string,
     pagination: PaginationModel,
     dataRange?: DataRangeModel,
-  ): TransferEntity[] {}
+  ): TransferEntity[] {
+    const currentTransfersOut =
+      this.transferRepository.findByOutcomeAccount(accountId);
+    const currentTransfersIn =
+      this.transferRepository.findByIncomeAccount(accountId);
+    const currentTransfers = [...currentTransfersIn, ...currentTransfersOut];
+    return this.historyPagination(currentTransfers, pagination, dataRange);
+  }
 
   deleteTransfer(transferId: string): void {
     this.transferRepository.delete(transferId, true);
+  }
+
+  private historyPagination(
+    transfersList: TransferEntity[],
+    pagination: PaginationModel,
+    dataRange?: DataRangeModel,
+  ): TransferEntity[] {
+    const transfers: TransferEntity[] = [];
+    let range = 0;
+    if (dataRange && dataRange.range > 0) {
+      range = dataRange.range;
+    } else {
+      range = 10;
+    }
+    pagination.pages = Math.round(pagination.size / range);
+    for (
+      let i = pagination.currentPage * range - range;
+      i < pagination.currentPage * range;
+      i++
+    ) {
+      transfers.push(transfersList[i]);
+    }
+    return transfers;
   }
 }
