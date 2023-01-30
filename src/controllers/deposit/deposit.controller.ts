abbcee54a7fe66eb35e7897ec0980700044f06aa
Julian362 <Juliangarcia362@hotmail.com>
2023-01-30 16:27:42 -0500

    Se implementa controller
    Se implementa el deposit controller con sus us metodos

--- src/controllers/deposit/deposit.controller.ts
+++ src/controllers/deposit/deposit.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+  } from '@nestjs/common';
+  import { DepositDTO } from 'src/dtos';
+  import { DepositEntity } from 'src/persistence/entities';
+  import { DepositService } from 'src/services';
 
 @Controller('deposit')
-export class DepositController {}
+export class DepositController {
+
+    @Post()
+    createDeposit(@Body() account: DepositDTO): DepositEntity {
+      return this.accountService.createAccount(account);
+    }
+
+    @Delete()
+    deleteDeposit(@Body() despositId: string): void {
+      this.accountService.deleteAccount(despositId);
+    }
+
+}
