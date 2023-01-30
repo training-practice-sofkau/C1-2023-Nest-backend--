d978c8fcabbe6f7df1d2cd0a376089078d1d9239
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-01-30 11:15:50 -0500

    controller - crear controler account

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SecurityController } from './controllers';
 import { AccountService } from './services/account/account.service';
 import { SecurityService } from './services/security/security.service';
+import { AccountController } from './controllers/account/account.controller';
 
 @Module({
   imports: [],
-  controllers: [SecurityController],
+  controllers: [SecurityController, AccountController],
   providers: [AccountService, SecurityService],
 })
 export class AppModule {}
