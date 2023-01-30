34ef6793288d8174effd24af373c4d1cce01525e
codeJ30 <juancode09@gmail.com>
2023-01-30 15:52:28 -0500

    TITULO: Update a customerService, DESCRIPCION : Se actualiza el servicio de customer y se crea el dtos para el mismo , estos cambios fueron realizadon por al coach Julian

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CustomerService } from '../../services/customer/customer.service';
+import { CustomerEntity } from '../../persistence/entities/customer.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor(private readonly customerService: CustomerService){}
+
+    @Get()
+    findAllUser(): CustomerEntity[] {
+        return this.customerService.findAll();
+    }
+}
+
+
+
+
+
--- src/services/customer/customer.service.ts
+++ src/services/customer/customer.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { NewCustomerDTO } from 'src/dtos/new-customer.dto';
 import { CustomerModel } from '../../models';
-import { CustomerEntity } from '../../persistence/entities';
+import { CustomerEntity, DocumentTypeEntity } from '../../persistence/entities';
 import { CustomerRepository } from '../../persistence/repositories/customer.repository';
 
 @Injectable()
 export class CustomerService {
+ 
   constructor(private readonly customerRepository: CustomerRepository) {}
+
+  findAll():CustomerEntity[]{
+    return this.customerRepository.findAll();
+  }
+
+  newCustomer(customer: NewCustomerDTO): CustomerEntity {
+    const documentType = new DocumentTypeEntity();
+    documentType.id = customer.documentTypeId;
+
+    const newCustomer = new CustomerEntity();
+    newCustomer.documentType = documentType;
+    newCustomer.document = customer.document;
+    newCustomer.fullName = customer.fullName;
+    newCustomer.email = customer.email;
+    newCustomer.phone = customer.phone;
+    newCustomer.password = customer.password;
+
+    return this.customerRepository.register(newCustomer)
+  }
   /**
    * Obtener información de un cliente
    *
