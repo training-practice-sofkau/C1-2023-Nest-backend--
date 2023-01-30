433a63e7b24cae41abf8f8a08ee2d3eafe7b00b2
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-01-30 13:32:13 -0500

    Se modifica el accountController
    Se crea el account Controller, se deja lista la estructura.

--- src/controllers/account/account.controller.ts
+++ src/controllers/account/account.controller.ts
@@ -1,29 +1,33 @@
 import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, Put } from "@nestjs/common";
+import { NewAccountDTO } from "src/dtos/account/new-account.dto";
+import { AccountEntity } from "src/persistence/entities";
+import { AccountService } from "src/services";
 
 @Controller("account")
 export class AccountController {
+
+    constructor(private readonly accountServices: AccountService) { }
     @Get()
-    getAccounts(): string {
+    getAccounts(): AccountEntity[] {
         return "Se devuelve todas las cuentas"
     }
 
     @Get(":id")
-    getAccount(@Param("id", new ParseUUIDPipe()) id: string): string {
+    getAccount(@Param("id", new ParseUUIDPipe()) id: string): AccountEntity {
         return "Devuelve la cuenta con id :" + id;
     }
 
     @Post(":id")
-    createAccount(@Param("id") id: string, @Body() account: {}): string {
+    createAccount(@Param("id", new ParseUUIDPipe()) id: string, @Body() account: NewAccountDTO): AccountEntity {
         return "Se crea cuenta con user id: " + id;
     }
 
     @Put(":id")
-    modifyAccount(@Param("id") id: string, @Body() account: {}): string {
+    modifyAccount(@Param("id", new ParseUUIDPipe()) id: string, @Body() account: NewAccountDTO): AccountEntity {
         return "Se modifica la cuenta con user id: " + id;
     }
 
     @Delete(":id")
-    deleteAccount(@Param("id") id: string): string {
-        return "Se elimina la cuenta con userID: " + id
+    deleteAccount(@Param("id", new ParseUUIDPipe()) id: string): void {
     }
 }
