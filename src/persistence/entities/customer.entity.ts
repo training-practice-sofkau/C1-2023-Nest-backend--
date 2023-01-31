46ce97d9a03ad15c9ba169b67bc890d9b2c4d8a6
codeJ30 <juancode09@gmail.com>
2023-01-30 23:01:09 -0500

    TITULO: Update al customer, DESCRIPCION: Se corrigen los errores prentados en la entidad customer (Modelo, repositorio e interfaz, se ejecuta el proyecto en postman)

--- src/persistence/entities/customer.entity.ts
+++ src/persistence/entities/customer.entity.ts
@@ -3,23 +3,15 @@ import { DocumentTypeEntity } from './';
 import { CustomerModel } from '../../models';
 
 export class CustomerEntity implements CustomerModel {
+  
   id = uuid();
-
   documentType: DocumentTypeEntity;
-
   document: string;
-
   fullName: string;
-
   email: string;
-
   phone: string;
-
   password: string;
-
   avatarUrl?: string;
-
   state = true;
-
-  deleteAt?: Date | number;
+  deletedAt?: number | Date ;
 }
