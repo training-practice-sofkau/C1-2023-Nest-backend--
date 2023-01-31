384544c26c739ab8610f6c7f23da998fed41cf01
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-01-31 12:36:35 -0500

    Se arregla los imports de AppModule
    Se arregla los imports de appModule para que funcione el BackEnd.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AccountModule } from './modules/account/account.module';
-import { CustomerModule } from './modules/customer/customer.module';
-import { DepositModule } from './modules/deposit/deposit.module';
-import { SecurityModule } from './modules/security/security.module';
-import { TransferModule } from './modules/transfer/transfer.module';
+import { AccountModule, CustomerModule, DepositModule, SecurityModule, TransferModule } from './data/modules';
 
 
 @Module({
--- src/data/modules/account/account.module.ts
+++ src/data/modules/account/account.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common/decorators/modules/module.decorator";
 import { AccountController } from "src/presentation";
-import { AccountTypeRepository } from "src/business/persistence";
-import { AccountRepository } from "src/business/persistence";
+import { AccountTypeRepository, AccountRepository } from "src/business/persistence";
 import { AccountService } from "src/data/services";
 
 @Module({
-    controllers:[AccountController],
-    providers:[AccountService,AccountRepository,AccountTypeRepository]
+    controllers: [AccountController],
+    providers: [AccountService, AccountRepository, AccountTypeRepository]
 })
-export class AccountModule{}
+export class AccountModule { }
