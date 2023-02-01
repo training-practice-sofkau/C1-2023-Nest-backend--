96b85a8cd1bdbe4a15c979eb061ab950bab7fcf9
migueltorresd <migueltorresdiaz1992@gmail.com>
2023-02-01 03:03:49 -0500

    Titulo: se crean controllers de security  -Descripcion: se crean controllers para security queda pendiente crear controller de salida del sistema

--- src/controllers/security/security.controller.ts
+++ src/controllers/security/security.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { NewCustomerDTO } from 'src/dtos';
 import { SecurityService } from 'src/services';
+import { NewSecurityDTO } from '../../dtos/new-security.dto';
+import { CustomerEntity } from '../../persistence/entities/customer.entity';
 
 @Controller('security')
 export class SecurityController {
   constructor(private readonly securityService: SecurityService) {}
+  //Crear usuario en el sistema
+  @Post('newUser')
+  signU(@Body() signUp: NewSecurityDTO): CustomerEntity {
+    return this.securityService.signUp(signUp);
+  }
   //Identificarse en el sistema
-  // @Post()
-  // verifyUser(@Body() security: NewCustomerDTO) {
-  //   return this.securityService.signIn(security);
-  // }
+  @Post()
+  signIn(@Body() signIn: NewSecurityDTO): string {
+    return this.securityService.signIn(signIn);
+  }
+  //Salir del sistema
 }
