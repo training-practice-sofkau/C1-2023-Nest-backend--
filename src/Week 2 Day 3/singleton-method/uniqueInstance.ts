b6fe12462fb8b1ec6774e120e69231385c8497fd
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-02-02 15:56:04 -0500

    Se crea el metodo Singleton
    Se crea el metodo SingleTon

---
+++ src/Week 2 Day 3/singleton-method/uniqueInstance.ts
@@ -0,0 +1,29 @@
+class OnlyInstance {
+    private static instance: OnlyInstance;
+    private saldo: number;
+    private constructor(saldo: number) {
+        this.saldo = saldo
+    }
+    public static getInstance(): OnlyInstance {
+        if (!OnlyInstance.instance) {
+            OnlyInstance.instance = new OnlyInstance(0)
+        }
+        return this.instance;
+    }
+
+    private getSaldo() {
+        return this.saldo;
+    }
+    private addSaldo(amount: number) {
+        this.saldo = this.saldo + amount
+        return this.saldo;
+    }
+    private removeSaldo(amount: number) {
+        if (this.saldo > amount) this.saldo = this.saldo - amount
+        return this.saldo
+    }
+}
+
+let uni = OnlyInstance;
+const only=uni.getInstance()
+console.log(only)
