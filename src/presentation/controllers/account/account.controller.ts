61f5205de09c34101f6f854ff9323bdf8d466c7f
Julian362 <Juliangarcia362@hotmail.com>
2023-02-03 07:59:52 -0500

    Se actualizan los controladores
    se actualizanlos controladores de:
    -account
    -deposit
    -security

--- src/presentation/controllers/account/account.controller.ts
+++ src/presentation/controllers/account/account.controller.ts
@@ -15,9 +15,14 @@ import { AccountService } from '../../../business/services';
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
-  @Get(':id')
+  @Get('balance/:id')
   getBalance(@Param('id') id: string): number {
     return this.accountService.getBalance(id);
+  }
+
+  @Get()
+  getAll(): AccountEntity[] {
+    return this.accountService.findAll();
   }
 
   @Post()
@@ -26,22 +31,59 @@ export class AccountController {
   }
 
   @Post(':id')
-  addBalance(@Param('id') id: string, @Body() amount: number): void {
-    this.accountService.addBalance(id, amount);
+  addBalance(
+    @Param('id') id: string,
+    @Body() body: { amount: number },
+  ): string {
+    if (body.amount < 0) return 'debe ser un numero positivo';
+    this.accountService.addBalance(id, body.amount);
+    return '' + this.accountService.getBalance(id);
   }
 
-  @Get(':id')
-  amountBalance(@Param('id') id: string, @Body() amount: number): boolean {
-    return this.accountService.verifyAmountIntoBalance(id, amount);
+  @Put('removebalance/:id')
+  removeBalance(
+    @Param('id') id: string,
+    @Body() body: { amount: number },
+  ): string {
+    if (body.amount < 0) return 'debe ser un numero positivo';
+    this.accountService.removeBalance(id, body.amount);
+    return '' + this.accountService.getBalance(id);
+  }
+
+  @Get('/balance/amount/:id')
+  amountBalance(
+    @Param('id') id: string,
+    @Body() body: { amount: number },
+  ): boolean {
+    return this.accountService.verifyAmountIntoBalance(id, body.amount);
   }
 
   @Put(':id')
-  changeState(@Param('id') id: string, @Body() state: boolean): void {
-    this.accountService.changeState(id, state);
+  changeState(
+    @Param('id') id: string,
+    @Body() body: { state: boolean },
+  ): string {
+    this.accountService.changeState(id, body.state);
+    return 'estado cambiado';
   }
 
-  @Delete()
-  deleteAccount(@Body() customerId: string): void {
-    this.accountService.deleteAccount(customerId);
+  @Get('state/:id')
+  getState(@Param('id') id: string): boolean {
+    return this.accountService.getState(id);
+  }
+
+  @Put('accountype/:id')
+  changeAccountType(
+    @Param('id') id: string,
+    @Body() body: { accountTypeId: string },
+  ): string {
+    this.accountService.changeAccountType(id, body.accountTypeId);
+    return 'cuenta cambiada';
+  }
+
+  @Delete(':id')
+  deleteAccount(@Param('id') id: string): string {
+    this.accountService.deleteAccount(id);
+    return 'cuenta eliminada';
   }
 }
--- src/presentation/controllers/security/security.controller.ts
+++ src/presentation/controllers/security/security.controller.ts
@@ -1,18 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CustomerDTO } from '../../../business/dtos';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SecurityService } from '../../../business/services';
+import { CustomerDTO, SignDTO } from '../../../business/dtos';
 
 @Controller('security')
 export class SecurityController {
   constructor(private readonly securityService: SecurityService) {}
 
-  @Post()
-  signUp(@Body() customer: CustomerDTO): string {
+  @Post('sign-up')
+  signUp(@Body() customer: CustomerDTO) {
     return this.securityService.signUp(customer);
   }
 
-  @Post()
-  signIn(@Body() customer: CustomerDTO): string {
+  @Post('sign-in')
+  signIn(@Body() customer: SignDTO) {
     return this.securityService.signIn(customer);
+  }
+
+  @Get('logout')
+  logout(@Body() body: { JWT: string }): boolean {
+    return this.securityService.signOut(body.JWT);
   }
 }
