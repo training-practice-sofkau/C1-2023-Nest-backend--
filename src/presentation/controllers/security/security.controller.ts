33075c8fb4d5c29fa41e5820bdc19abd50fbad32
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-02-04 18:55:47 -0500

    jwtGuard
    Se crea el archivo guard del JWT

--- src/presentation/controllers/security/security.controller.ts
+++ src/presentation/controllers/security/security.controller.ts
@@ -1,13 +1,15 @@
 // Libraries
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { newCustomerDTO, NewSecurityDTO } from 'src/business/dtos';
 import { SecurityService } from 'src/business/services';
+import { JwtGuard } from 'src/presentation/guards/guard-jwt';
 
 @Controller('security')
 export class SecurityController {
     constructor(private readonly securityService: SecurityService) { }
 
     @Post("/login")
+    @UseGuards(JwtGuard)
     login(@Body() security: NewSecurityDTO) {
         return this.securityService.signIn(security)
     }
